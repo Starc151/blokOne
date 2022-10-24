15cdfebf82c51a8fbbe7bf31c2c606f23ddef2d1
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-24 23:45:47 +0300

    reading and output of the initial array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using static System.Console;
 Clear();
 
+Write("Введите массив строк через \",\" или пробел: ");
+string[] arr=ReadLine()!.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
 
+
+Write(string.Join(", ", arr));
