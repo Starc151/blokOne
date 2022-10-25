fe149b2279e13bb96a39e472e0881f8856c6d8f6
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-25 20:27:13 +0300

    complete

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿using static System.Console;
 Clear();
-string GetStringArrMoreThree(string[] inArr){
-    string str="";
-    foreach(string i in inArr){
-        if(i.Length<=3)str+=i;
-    }
 
-    return str;
+string[] GetStringArrMoreThree(string[] inArr){
+    string tmpStr="";
+    foreach(string i in inArr){
+        if(i.Length<=3)tmpStr+=i+" ";
+    }
+    return tmpStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 }
 Write("Введите массив строк через \",\" или пробел: ");
 string[] arr=ReadLine()!.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
 Write(string.Join(", ", arr));
 WriteLine();
-WriteLine(GetStringArrMoreThree(arr));
+WriteLine(string.Join(", ", GetStringArrMoreThree(arr)));
