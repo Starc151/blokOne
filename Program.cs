45d3bfb7625ff2e6ba89cb3161d1ee72d8003e4d
AlekseyP <dr.Starc@ya.r>
2022-10-25 16:24:06 +0300

    reassembly string

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 ﻿using static System.Console;
 Clear();
+string GetStringArrMoreThree(string[] inArr){
+    string str="";
+    foreach(string i in inArr){
+        if(i.Length<=3)str+=i;
+    }
 
+    return str;
+}
 Write("Введите массив строк через \",\" или пробел: ");
 string[] arr=ReadLine()!.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
-
-
 Write(string.Join(", ", arr));
+WriteLine();
+WriteLine(GetStringArrMoreThree(arr));
